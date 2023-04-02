7100c32b4a8a388c78ee6eaa0822c9b68dccadc5
nunomst <nunoteixeiradev@gmail.com>
2023-04-02 13:15:38 +0100

    Add Empresa

---
+++ Empresa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Globalization;
+
+
+namespace ND_Auto
+{
+    internal class Empresa
+    {
+        List<string> ListaVeiculos = new List<string>();
+
+        public Empresa()
+        {
+            //ListaVeiculos.Add("Carro");
+        }
+    }
+}
